dcafd90068860c2eb7bb3f791e54f067109f248a
KonstantinBelenko <konstantinbelenko36@gmail.com>
2021-12-09 00:09:38 +0300

    config changed

--- config.py
+++ config.py
@@ -1,10 +1,14 @@
 bot_token = ""
 
-# Enter default admin users here
+# Enter default admin users here (at least one)
 admin_users = [
-
+    
 ]
 
+# Default links
 website_links =[
-    "https://www.google.com", 
+    "https://www.google.com"
 ]
+
+# Default schedule (in minutes)
+def_schedule = 15
